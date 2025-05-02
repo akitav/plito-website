3c55235fa30950e71d15322f5995bf23fc7e9374
akitav <akitav@paideiaacademy.in>
2025-05-02 17:14:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+export default {
+  base: '/plito-website/',
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
